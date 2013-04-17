b1db434f4228bc64eaf171ffbfb2c23e1ad5fbb8
pspiroib <pspiro@interactivebrokers.com>
2013-04-17 17:56:12 -0400

    add formatting

--- source/javaclient/com/ib/contracts/ComboContract.java
+++ source/javaclient/com/ib/contracts/ComboContract.java
@@ -7,26 +7,24 @@ import com.ib.client.Contract;
 
 public class ComboContract extends Contract {
 
-   public ComboContract(String symbol) {
-      m_symbol = symbol;
-      m_secType = "BAG";
-      m_currency = "USD";
-      m_exchange = "SMART";
-   }
+	public ComboContract(String symbol) {
+		m_symbol = symbol;
+		m_secType = "BAG";
+		m_currency = "USD";
+		m_exchange = "SMART";
+	}
 
-   public ComboContract(String symbol, String currency) {
-      m_symbol = symbol;
-      m_secType = "BAG";
-      m_currency = currency;
-      m_exchange = "SMART";
-   }
+	public ComboContract(String symbol, String currency) {
+		m_symbol = symbol;
+		m_secType = "BAG";
+		m_currency = currency;
+		m_exchange = "SMART";
+	}
 
-   public ComboContract(String symbol, String currency, String exchange) {
-      m_symbol = symbol;
-      m_secType = "BAG";
-      m_currency = currency;
-      m_exchange = exchange;
-   }
+	public ComboContract(String symbol, String currency, String exchange) {
+		m_symbol = symbol;
+		m_secType = "BAG";
+		m_currency = currency;
+		m_exchange = exchange;
+	}
 }
-
-
--- source/javaclient/com/ib/contracts/StkContract.java
+++ source/javaclient/com/ib/contracts/StkContract.java
@@ -7,11 +7,10 @@ import com.ib.client.Contract;
 
 public class StkContract extends Contract {
 
-   public StkContract(String symbol) {
-      m_symbol = symbol;
-      m_secType = "STK";
-      m_exchange = "SMART";
-      m_currency = "USD";
-   }
+	public StkContract(String symbol) {
+		m_symbol = symbol;
+		m_secType = "STK";
+		m_exchange = "SMART";
+		m_currency = "USD";
+	}
 }
-
