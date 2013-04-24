c26eef43b29937a80cbc2e921a0c103c84072319
pspiroib <pspiro@interactivebrokers.com>
2013-04-24 11:43:57 -0400

    Fix EOL

--- samples/Java/TestJavaClient/AcctUpdatesDlg.java
+++ samples/Java/TestJavaClient/AcctUpdatesDlg.java
@@ -1,7 +1,8 @@
 /* Copyright (C) 2013 Interactive Brokers LLC. All rights reserved.  This code is subject to the terms
  * and conditions of the IB API Non-Commercial License or the IB API Commercial License, as applicable. */
 
-package TestJavaClient;
+
+package TestJavaClient;
 
 import java.awt.BorderLayout;
 import java.awt.event.ActionEvent;
