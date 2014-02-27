1aec092d57fc08c9d45055e74ca5d615365af77a
mvorobyevib <mvorobyev@interactivebrokers.com>
2014-02-27 16:22:53 +0400

    newline after licens head

--- source/csharpclient/Properties/AssemblyInfo.cs
+++ source/csharpclient/Properties/AssemblyInfo.cs
@@ -1,5 +1,6 @@
 /* Copyright (C) 2013 Interactive Brokers LLC. All rights reserved.  This code is subject to the terms
  * and conditions of the IB API Non-Commercial License or the IB API Commercial License, as applicable. */
+
 using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
--- source/csharpclient/TWSApi/Tws.cs
+++ source/csharpclient/TWSApi/Tws.cs
@@ -1,5 +1,6 @@
 /* Copyright (C) 2013 Interactive Brokers LLC. All rights reserved.  This code is subject to the terms
  * and conditions of the IB API Non-Commercial License or the IB API Commercial License, as applicable. */
+
 using IBApi;
 using Microsoft.Win32;
 using System;
