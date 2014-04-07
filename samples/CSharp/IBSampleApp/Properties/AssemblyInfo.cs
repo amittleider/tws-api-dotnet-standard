f90488accf1339b61b4be7a5411b7b139b178b80
pspiroib <pspiro@interactivebrokers.com>
2014-04-07 13:35:40 -0400

    remove internal BOM

--- samples/CSharp/IBSampleApp/Properties/AssemblyInfo.cs
+++ samples/CSharp/IBSampleApp/Properties/AssemblyInfo.cs
@@ -1,7 +1,7 @@
 /* Copyright (C) 2013 Interactive Brokers LLC. All rights reserved.  This code is subject to the terms
  * and conditions of the IB API Non-Commercial License or the IB API Commercial License, as applicable. */
 
-﻿using System.Reflection;
+using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
