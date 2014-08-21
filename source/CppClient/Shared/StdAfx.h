e19f6b75b78b33ad2fff2d7fc990d2d3133d5325
Nick Orlov <norlov@interactivebrokers.com>
2014-08-21 16:35:43 -0400

    make DDLEXP an empty define for non-Windows builds

--- source/CppClient/Shared/StdAfx.h
+++ source/CppClient/Shared/StdAfx.h
@@ -1,14 +1,18 @@
 /* Copyright (C) 2013 Interactive Brokers LLC. All rights reserved. This code is subject to the terms
  * and conditions of the IB API Non-Commercial License or the IB API Commercial License, as applicable. */
 
+#ifdef _MSC_VER
+
 #ifndef DLLEXP
 #define DLLEXP __declspec( dllexport )
 #endif
 
-#ifdef _MSC_VER
-
 #define assert ASSERT
 #define snprintf _snprintf
+
+#else
+
+#define DLLEXP
 
 #endif
 
