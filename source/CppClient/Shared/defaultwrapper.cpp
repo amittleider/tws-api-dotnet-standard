46dbf8bdc384eccf4b3ce5c08f570adedb532114
mvorobyevib <mvorobyev@interactivebrokers.com>
2014-10-02 21:52:25 +0400

    add license header

--- source/CppClient/Shared/defaultwrapper.cpp
+++ source/CppClient/Shared/defaultwrapper.cpp
@@ -1,4 +1,7 @@
-﻿#include "StdAfx.h"
+﻿/* Copyright (C) 2013 Interactive Brokers LLC. All rights reserved. This code is subject to the terms
+ * and conditions of the IB API Non-Commercial License or the IB API Commercial License, as applicable. */
+
+#include "StdAfx.h"
 #include "DefaultWrapper.h"
 
 void DefaultWrapper::tickPrice( TickerId tickerId, TickType field, double price, int canAutoExecute) { }
