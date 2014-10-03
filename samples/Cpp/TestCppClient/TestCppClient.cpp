06cde1da3d90a37f6ee04927e73dce6de8d2849f
mvorobyevib <mvorobyev@interactivebrokers.com>
2014-10-03 18:32:32 +0400

    tabs to spaces

--- samples/Cpp/TestCppClient/TestCppClient.cpp
+++ samples/Cpp/TestCppClient/TestCppClient.cpp
@@ -160,8 +160,8 @@ void TestCppClient::cancelOrder()
 ///////////////////////////////////////////////////////////////////
 // events
 void TestCppClient::orderStatus( OrderId orderId, const std::string& status, int filled,
-								int remaining, double avgFillPrice, int permId, int parentId,
-								double lastFillPrice, int clientId, const std::string& whyHeld)
+	                            int remaining, double avgFillPrice, int permId, int parentId,
+	                            double lastFillPrice, int clientId, const std::string& whyHeld)
 
 {
 	if( orderId == m_orderId) {
@@ -207,21 +207,21 @@ void TestCppClient::error(const int id, const int errorCode, const std::string e
 void TestCppClient::tickPrice( TickerId tickerId, TickType field, double price, int canAutoExecute) {}
 void TestCppClient::tickSize( TickerId tickerId, TickType field, int size) {}
 void TestCppClient::tickOptionComputation( TickerId tickerId, TickType tickType, double impliedVol, double delta,
-										  double optPrice, double pvDividend,
-										  double gamma, double vega, double theta, double undPrice) {}
+                                          double optPrice, double pvDividend,
+                                          double gamma, double vega, double theta, double undPrice) {}
 void TestCppClient::tickGeneric(TickerId tickerId, TickType tickType, double value) {}
 void TestCppClient::tickString(TickerId tickerId, TickType tickType, const std::string& value) {}
 void TestCppClient::tickEFP(TickerId tickerId, TickType tickType, double basisPoints, const std::string& formattedBasisPoints,
-							double totalDividends, int holdDays, const std::string& futureExpiry, double dividendImpact, double dividendsToExpiry) {}
+                            double totalDividends, int holdDays, const std::string& futureExpiry, double dividendImpact, double dividendsToExpiry) {}
 void TestCppClient::openOrder( OrderId orderId, const Contract&, const Order&, const OrderState& ostate) {}
 void TestCppClient::openOrderEnd() {}
 void TestCppClient::winError( const std::string& str, int lastError) {}
 void TestCppClient::connectionClosed() {}
 void TestCppClient::updateAccountValue(const std::string& key, const std::string& val,
-									   const std::string& currency, const std::string& accountName) {}
+                                       const std::string& currency, const std::string& accountName) {}
 void TestCppClient::updatePortfolio(const Contract& contract, int position,
-									double marketPrice, double marketValue, double averageCost,
-									double unrealizedPNL, double realizedPNL, const std::string& accountName){}
+                                    double marketPrice, double marketValue, double averageCost,
+                                    double unrealizedPNL, double realizedPNL, const std::string& accountName){}
 void TestCppClient::updateAccountTime(const std::string& timeStamp) {}
 void TestCppClient::accountDownloadEnd(const std::string& accountName) {}
 void TestCppClient::contractDetails( int reqId, const ContractDetails& contractDetails) {}
@@ -231,21 +231,21 @@ void TestCppClient::execDetails( int reqId, const Contract& contract, const Exec
 void TestCppClient::execDetailsEnd( int reqId) {}
 
 void TestCppClient::updateMktDepth(TickerId id, int position, int operation, int side,
-								   double price, int size) {}
+                                   double price, int size) {}
 void TestCppClient::updateMktDepthL2(TickerId id, int position, std::string marketMaker, int operation,
-									 int side, double price, int size) {}
+                                     int side, double price, int size) {}
 void TestCppClient::updateNewsBulletin(int msgId, int msgType, const std::string& newsMessage, const std::string& originExch) {}
 void TestCppClient::managedAccounts( const std::string& accountsList) {}
 void TestCppClient::receiveFA(faDataType pFaDataType, const std::string& cxml) {}
 void TestCppClient::historicalData(TickerId reqId, const std::string& date, double open, double high,
-								   double low, double close, int volume, int barCount, double WAP, int hasGaps) {}
+                                   double low, double close, int volume, int barCount, double WAP, int hasGaps) {}
 void TestCppClient::scannerParameters(const std::string& xml) {}
 void TestCppClient::scannerData(int reqId, int rank, const ContractDetails& contractDetails,
-								const std::string& distance, const std::string& benchmark, const std::string& projection,
-								const std::string& legsStr) {}
+                                const std::string& distance, const std::string& benchmark, const std::string& projection,
+                                const std::string& legsStr) {}
 void TestCppClient::scannerDataEnd(int reqId) {}
 void TestCppClient::realtimeBar(TickerId reqId, long time, double open, double high, double low, double close,
-								long volume, double wap, int count) {}
+                                long volume, double wap, int count) {}
 void TestCppClient::fundamentalData(TickerId reqId, const std::string& data) {}
 void TestCppClient::deltaNeutralValidation(int reqId, const UnderComp& underComp) {}
 void TestCppClient::tickSnapshotEnd(int reqId) {}
