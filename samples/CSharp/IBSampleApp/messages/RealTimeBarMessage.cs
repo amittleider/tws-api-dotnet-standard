906df6372ce8a2cdbbf9e82e877539ad2b0d9c58
rlktradewright <rlking@aultan.com>
2015-10-29 11:19:52 +0000

    C# sample app - include realtimebar display

--- samples/CSharp/IBSampleApp/messages/RealTimeBarMessage.cs
+++ samples/CSharp/IBSampleApp/messages/RealTimeBarMessage.cs
@@ -25,11 +25,17 @@ namespace IBSampleApp.messages
         }
 
         public RealTimeBarMessage(int reqId, long date, double open, double high, double low, double close, long volume, double WAP, int count)
-            : base(reqId, "", open, high, low, close, -1, count, WAP, false)
+            : base(reqId, UnixTimestampToDateTime(date).ToString("yyyyMMdd hh:mm:ss"), open, high, low, close, -1, count, WAP, false)
         {
             Type = MessageType.RealTimeBars;
             Timestamp = date;
             LongVolume = volume;
+        }
+
+        static DateTime UnixTimestampToDateTime(long unixTimestamp)
+        {
+            DateTime unixBaseTime = new DateTime(1970, 1, 1, 0, 0, 0, 0);
+            return unixBaseTime.AddSeconds(unixTimestamp);
         }
     }
 }
