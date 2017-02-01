b90813b520a3aa56f38a6e01bc34f0ed0eb7c6af
s-klokov <s-klokov@yandex.ru>
2017-02-01 11:24:05 +0600

    ContractDescription.java, Types.java: use defensive copying.

--- source/javaclient/com/ib/client/ContractDescription.java
+++ source/javaclient/com/ib/client/ContractDescription.java
@@ -10,30 +10,47 @@ public class ContractDescription {
     private String[] m_derivativeSecTypes;
 
     // Get
-    public Contract contract() { return m_contract; }
-    public String[] derivativeSecTypes() { return m_derivativeSecTypes; }
+    public Contract contract() {
+        return m_contract;
+    }
+
+    public String[] derivativeSecTypes() {
+        return (m_derivativeSecTypes == null) ? null : Arrays.copyOf(m_derivativeSecTypes, m_derivativeSecTypes.length);
+    }
 
     // Set
-    public void contract(Contract contract) { m_contract = contract; }
-    public void derivativeSecTypes(String[] derivativeSecTypes) { m_derivativeSecTypes = derivativeSecTypes; }
+    public void contract(Contract contract) {
+        m_contract = contract;
+    }
+
+    public void derivativeSecTypes(String[] derivativeSecTypes) {
+        if (derivativeSecTypes == null) {
+            m_derivativeSecTypes = null;
+        } else {
+            m_derivativeSecTypes = Arrays.copyOf(derivativeSecTypes, derivativeSecTypes.length);
+        }
+    }
 
     public ContractDescription() {
         m_contract = new Contract();
     }
 
-    public ContractDescription( Contract p_contract, String[] p_derivativeSecTypes) {
+    public ContractDescription(Contract p_contract, String[] p_derivativeSecTypes) {
         m_contract = p_contract;
-        m_derivativeSecTypes = p_derivativeSecTypes;
+        if (p_derivativeSecTypes == null) {
+            m_derivativeSecTypes = null;
+        } else {
+            m_derivativeSecTypes = Arrays.copyOf(p_derivativeSecTypes, p_derivativeSecTypes.length);
+        }
     }
 
-    @Override public String toString() {
-        StringBuilder sb = new StringBuilder();
-        sb.append( "conid: " + m_contract.conid() + "\n");
-        sb.append( "symbol: " + m_contract.symbol() + "\n");
-        sb.append( "secType: " + m_contract.secType().toString() + "\n");
-        sb.append( "primaryExch: " + m_contract.primaryExch() + "\n");
-        sb.append( "currency: " + m_contract.currency() + "\n");
-        sb.append( "derivativeSecTypes: " + Arrays.toString(m_derivativeSecTypes) + "\n");
-        return sb.toString();
+    @Override
+    public String toString() {
+        return "conid: " + m_contract.conid() + "\n"
+                + "symbol: " + m_contract.symbol() + "\n"
+                + "secType: " + m_contract.secType().toString() + "\n"
+                + "primaryExch: " + m_contract.primaryExch() + "\n"
+                + "currency: " + m_contract.currency() + "\n"
+                + "derivativeSecTypes: " + Arrays.toString(m_derivativeSecTypes) + "\n";
     }
 }
