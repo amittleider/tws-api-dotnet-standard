f28903ce2620300739d42d4e036f017c6efb0a12
jbeacomib <jbeacomib@interactivebrokers.com>
2017-02-23 10:23:14 -0600

    Docs updated for new Python functions

--- options.js
+++ options.js
@@ -1,6 +1,7 @@
 var options =
 [
     [ "Option Chains", "options.html#option_chains", null ],
+    [ "Exercising options", "options.html#option_exercising", null ],
     [ "Option Greeks", "option_computations.html", [
       [ "Market data related to options", "option_computations.html#option_greeks", null ],
       [ "Calculating option prices and historical volatility", "option_computations.html#opt_calculations", null ]
