0f1f7555ca5fbf1e877f49c0acfeb6c525063c60
Michael Vorobyev <mvorobyev@interactivebrokers.com>
2017-07-25 21:42:12 +0300

    Introduce new ADJUSTED_LAST source price

--- samples/CSharp/IBSampleApp/ui/IBSampleApp.Designer.cs
+++ samples/CSharp/IBSampleApp/ui/IBSampleApp.Designer.cs
@@ -6290,7 +6290,8 @@ namespace IBSampleApp
             "YIELD_BID",
             "YIELD_ASK",
             "YIELD_BID_ASK",
-            "YIELD_LAST"});
+            "YIELD_LAST",
+            "ADJUTSTED_LAST"});
             this.hdRequest_WhatToShow.Location = new System.Drawing.Point(59, 92);
             this.hdRequest_WhatToShow.Name = "hdRequest_WhatToShow";
             this.hdRequest_WhatToShow.Size = new System.Drawing.Size(156, 20);
