ef5e2caef0483222ebb534f4b1ed4fac93de8a9f
sjinib <sjin@interactivebrokers.com>
2017-09-15 10:28:05 -0500

    Doc: RTD option greeks

---
+++ rtd_syntax.js
@@ -0,0 +1,28 @@
+var rtd_syntax =
+[
+    [ "Simple Syntax", "rtd_syntax.html#rtd_simple_syntax", [
+      [ "Ticker", "rtd_syntax.html#rtd_simple_syntax_ticker", null ],
+      [ "Topic", "rtd_syntax.html#rtd_simple_syntax_topic", null ],
+      [ "Connection Parameters", "rtd_syntax.html#rtd_simple_syntax_conn", null ]
+    ] ],
+    [ "Complex Syntax", "rtd_syntax.html#rtd_complex_syntax", null ],
+    [ "Mixed Syntax", "rtd_syntax.html#rtd_mixed_syntax", null ],
+    [ "TWS RTD Server Samples", "rtd_examples.html", [
+      [ "Forex Pairs", "rtd_examples.html#rtd_samples_fx", null ],
+      [ "Stocks", "rtd_examples.html#rtd_samples_stk", null ],
+      [ "Indexes", "rtd_examples.html#rtd_samples_ind", null ],
+      [ "CFDs", "rtd_examples.html#rtd_samples_cfd", null ],
+      [ "Futures", "rtd_examples.html#rtd_samples_fut", null ],
+      [ "Options", "rtd_examples.html#rtd_samples_opt", null ],
+      [ "Futures on Options", "rtd_examples.html#rtd_samples_fop", null ],
+      [ "Bonds", "rtd_examples.html#rtd_samples_bond", null ],
+      [ "Mutual Funds", "rtd_examples.html#rtd_samples_fund", null ],
+      [ "Commodities", "rtd_examples.html#rtd_samples_cmdty", null ],
+      [ "Spreads", "rtd_examples.html#rtd_samples_spread", [
+        [ "Stock Spread", "rtd_examples.html#rtd_samples_spread_stk", null ],
+        [ "Futures Spread", "rtd_examples.html#rtd_samples_spread_fut", null ],
+        [ "Options Spread", "rtd_examples.html#rtd_samples_spread_opt", null ],
+        [ "Inter-Commodity Futures Spread", "rtd_examples.html#rtd_samples_spread_intercmdty", null ]
+      ] ]
+    ] ]
+];
---
+++ search/all_69.js
@@ -0,0 +1,17 @@
+var searchData=
+[
+  ['introducing_20ewrapper_20and_20eclientsocket',['Introducing EWrapper and EClientSocket',['../client_wrapper.html',1,'']]],
+  ['ib_20bulletins',['IB Bulletins',['../ib_bulletins.html',1,'']]],
+  ['ib_20algorithms',['IB Algorithms',['../ibalgos.html',1,'algos']]],
+  ['ibapi',['IBApi',['../namespaceIBApi.html',1,'']]],
+  ['includeexpired',['IncludeExpired',['../classIBApi_1_1Contract.html#a871739ededda61657cd2482aaf6c8449',1,'IBApi::Contract']]],
+  ['industry',['Industry',['../classIBApi_1_1ContractDetails.html#a978bffaddccf5ab8aeb8736b76d0e4cb',1,'IBApi::ContractDetails']]],
+  ['initial_20setup',['Initial Setup',['../initial_setup.html',1,'']]],
+  ['initmargin',['InitMargin',['../classIBApi_1_1OrderState.html#a9d8863de4476165c245cd719c9c7a361',1,'IBApi::OrderState']]],
+  ['instrument',['Instrument',['../classIBApi_1_1ScannerSubscription.html#aeb2a08bf9851fc7628ec54099a945373',1,'IBApi::ScannerSubscription']]],
+  ['introduction',['Introduction',['../introduction.html',1,'']]],
+  ['isconnected',['IsConnected',['../classIBApi_1_1EClient.html#ab8e2702adca8f47228f9754f4963455d',1,'IBApi::EClient']]],
+  ['ispeggedchangeamountdecrease',['IsPeggedChangeAmountDecrease',['../classIBApi_1_1Order.html#a21f6d5a75fbb289b314722dae3701b56',1,'IBApi::Order']]],
+  ['issuedate',['IssueDate',['../classIBApi_1_1ContractDetails.html#a58fa4bd5c8490afb2107aa31e96bafb8',1,'IBApi::ContractDetails']]],
+  ['issuesignal',['issueSignal',['../interfaceIBApi_1_1EReaderSignal.html#a24e6831d25706462ef7fc7051363a647',1,'IBApi::EReaderSignal']]]
+];
