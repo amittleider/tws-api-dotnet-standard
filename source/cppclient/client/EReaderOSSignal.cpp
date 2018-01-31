amittleider/tws-api-dotnet-standard

commit e02ec65d237f571eff35d82eff7c30b152b4c577

[C++] use monotonic time for pthread_cond_timedwait(clock_gettime is not available on MacOSX, use gettimeofday on MacOSX for the moment)

source/cppclient/client/EReaderOSSignal.cpp
@@ -4,7 +4,11 @@
 #include "StdAfx.h"
 #include "EReaderOSSignal.h"
 #if defined(IB_POSIX)
+#if defined(__MACH__)
 #include <sys/time.h>
+#else
+#include <time.h>
+#endif
 #endif
 
 #define MS_IN_SEC 1000
@@ -17,7 +21,15 @@ EReaderOSSignal::EReaderOSSignal(unsigned long waitTimeout) throw (std::runtime_
 #if defined(IB_POSIX)
     int rc1 = pthread_mutex_init(&m_mutex, NULL);
     int rc2 = pthread_cond_init(&m_evMsgs, NULL);
+#if defined(__MACH__)
     ok = rc1 == 0 && rc2 == 0;
+#else
+    pthread_condattr_t attr;
+    int rc3 = pthread_condattr_init(&attr);
+    int rc4 = pthread_condattr_setclock(&attr, CLOCK_MONOTONIC);
+    pthread_condattr_destroy(&attr);
+    ok = rc1 == 0 && rc2 == 0 && rc3 == 0 && rc4 == 0;
+#endif
     open = false; 
 #elif defined(IB_WIN32)
 	m_evMsgs = CreateEvent(0, false, false, 0);
@@ -60,17 +72,27 @@ void EReaderOSSignal::waitForSignal() {
 #if defined(IB_POSIX)
     pthread_mutex_lock(&m_mutex); 
     if (!open) {
-	if ( m_waitTimeout == INFINITE ) {
-		pthread_cond_wait(&m_evMsgs, &m_mutex);
-	}
-	else {
-		struct timespec ts;
-		struct timeval tv;
-		gettimeofday(&tv, NULL);
-		ts.tv_sec = tv.tv_sec+m_waitTimeout/MS_IN_SEC;
-		ts.tv_nsec = (m_waitTimeout%MS_IN_SEC)*1000/*us/ms*/*1000/*ns/us*/;
-		pthread_cond_timedwait(&m_evMsgs, &m_mutex, &ts);
-	}
+        if ( m_waitTimeout == INFINITE ) {
+            pthread_cond_wait(&m_evMsgs, &m_mutex);
+        }
+        else {
+#if defined(__MACH__)
+// on Mac OS X, clock_gettime is not available, stick to gettimeofday for the moment
+            struct timespec ts;
+            struct timeval tv;
+            gettimeofday(&tv, NULL);
+            ts.tv_sec = tv.tv_sec+m_waitTimeout/MS_IN_SEC;
+            ts.tv_nsec = (m_waitTimeout%MS_IN_SEC)*1000/*us/ms*/*1000/*ns/us*/;
+#else
+            struct timespec ts;
+            clock_gettime(CLOCK_MONOTONIC, &ts);
+            ts.tv_sec += m_waitTimeout/ MS_IN_SEC;
+            ts.tv_nsec += MS_IN_SEC * MS_IN_SEC * (m_waitTimeout % MS_IN_SEC);
+            ts.tv_sec += ts.tv_nsec / (MS_IN_SEC * MS_IN_SEC * MS_IN_SEC);
+            ts.tv_nsec %= (MS_IN_SEC * MS_IN_SEC * MS_IN_SEC);
+#endif
+            pthread_cond_timedwait(&m_evMsgs, &m_mutex, &ts);
+        }
     }
     open = false;
     pthread_mutex_unlock(&m_mutex);

commit 51e63e5e9f528995b1c620f6023b63a318a59e3e

EReaderOSSignal: fix monotonic time usage
This was broken since introduced in e02ec65d because
attr was not used for pthread_cond_init().

Moreover we avoid destroying pthread_condattr before pthread_cond.
It's unclear if this would be defined behavior.

BTW we cleanup the return value handling to avoid calling again pthread_*
functions after one failed already.

CC: Yinbin Xu <yinbin.xu@it-finance.com>
Signed-off-by: Ruediger Meier <ruediger.meier@ga-group.nl>

source/cppclient/client/EReaderOSSignal.cpp
@@ -1,4 +1,4 @@
-﻿/* Copyright (C) 2013 Interactive Brokers LLC. All rights reserved. This code is subject to the terms
+/* Copyright (C) 2013 Interactive Brokers LLC. All rights reserved. This code is subject to the terms
  * and conditions of the IB API Non-Commercial License or the IB API Commercial License, as applicable. */
 
 #include "StdAfx.h"
@@ -11,23 +11,17 @@
 #endif
 #endif
 
-
 EReaderOSSignal::EReaderOSSignal(unsigned long waitTimeout) throw (std::runtime_error)
 {
-    bool ok = false;
+    bool ok = true;
     m_waitTimeout = waitTimeout;
 #if defined(IB_POSIX)
-    int rc1 = pthread_mutex_init(&m_mutex, NULL);
-    int rc2 = pthread_cond_init(&m_evMsgs, NULL);
+    ok = ok && !pthread_mutex_init(&m_mutex, NULL);
+    ok = ok && !pthread_condattr_init(&m_condattr);
 #if defined(IBAPI_MONOTONIC_TIME)
-    pthread_condattr_t attr;
-    int rc3 = pthread_condattr_init(&attr);
-    int rc4 = pthread_condattr_setclock(&attr, CLOCK_MONOTONIC);
-    pthread_condattr_destroy(&attr);
-    ok = rc1 == 0 && rc2 == 0 && rc3 == 0 && rc4 == 0;
-#else
-    ok = rc1 == 0 && rc2 == 0;
+    ok = ok && !pthread_condattr_setclock(&m_condattr, CLOCK_MONOTONIC);
 #endif
+    ok = ok && !pthread_cond_init(&m_evMsgs, &m_condattr);
     open = false; 
 #elif defined(IB_WIN32)
 	m_evMsgs = CreateEvent(0, false, false, 0);
@@ -44,6 +38,7 @@ EReaderOSSignal::~EReaderOSSignal(void)
 {
 #if defined(IB_POSIX)
     pthread_cond_destroy(&m_evMsgs);
+    pthread_condattr_destroy(&m_condattr);
     pthread_mutex_destroy(&m_mutex);
 #elif defined(IB_WIN32)
 	CloseHandle(m_evMsgs);

source/cppclient/client/EReaderOSSignal.h
@@ -1,4 +1,4 @@
-﻿/* Copyright (C) 2013 Interactive Brokers LLC. All rights reserved. This code is subject to the terms
+/* Copyright (C) 2013 Interactive Brokers LLC. All rights reserved. This code is subject to the terms
  * and conditions of the IB API Non-Commercial License or the IB API Commercial License, as applicable. */
 
 #pragma once
@@ -14,6 +14,7 @@ class TWSAPIDLLEXP EReaderOSSignal :
 	public EReaderSignal
 {
 #if defined(IB_POSIX)
+    pthread_condattr_t m_condattr;
     pthread_cond_t m_evMsgs;
     pthread_mutex_t m_mutex;
     bool open;