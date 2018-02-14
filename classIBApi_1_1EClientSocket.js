5db9a43883993ce5808c22ffcb79b1d621e06c12
sjinib <sjin@interactivebrokers.com>
2018-02-14 15:25:18 -0600

    Doc: Add avgOptVol and Delayed Last Timestamp

--- classIBApi_1_1EClientSocket.js
+++ classIBApi_1_1EClientSocket.js
@@ -5,6 +5,7 @@ var classIBApi_1_1EClientSocket =
     [ "createClientStream", "classIBApi_1_1EClientSocket.html#a5833b068af637df7ddceba2f8316a3ac", null ],
     [ "eConnect", "classIBApi_1_1EClientSocket.html#a315a7f7a34afc504d84c4f0ca462d924", null ],
     [ "eConnect", "classIBApi_1_1EClientSocket.html#ac46dbcbbc7230ff93a8fe26245909ac3", null ],
+    [ "eDisconnect", "classIBApi_1_1EClientSocket.html#a1adc34141d433d5bc55fcb7d330eace0", null ],
     [ "prepareBuffer", "classIBApi_1_1EClientSocket.html#a4d0cb0e9dee74db6b2a96df0d03ade13", null ],
     [ "redirect", "classIBApi_1_1EClientSocket.html#ae88f2c8c05758ef7718269691a9d424b", null ]
 ];
